449fddbf7de8670d7ca44d6fa65cf5cf3b26def7
DamianoSamperi <102314677+DamianoSamperi@users.noreply.github.com>
2024-12-17 11:45:38 +0100

    Add files via upload

--- jetson-exporter.py
+++ jetson-exporter.py
@@ -1,33 +1,9 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-
-# MIT License
-#
-# Copyright (c) 2021 Stefan von Cavallar
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-# 
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-# 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
+from prometheus_client import REGISTRY, start_http_server
+from prometheus_client.core import GaugeMetricFamily, InfoMetricFamily
 import time
 import atexit
 import argparse
 import logging
-from prometheus_client import start_http_server
-from prometheus_client.core import InfoMetricFamily, GaugeMetricFamily, REGISTRY
 from jtop import jtop, JtopException
 
 # Configurazione logging
@@ -36,9 +12,10 @@ logging.info('Server started on port %s', 9401)
 
 # Classe per raccogliere le metriche
 class CustomCollector(object):
-    def __init__(self):
+    def __init__(self, update_period=1):
         atexit.register(self.cleanup)
         self._jetson = jtop()
+        self._update_period = update_period
         try:
             self._jetson.start()
         except JtopException as e:
@@ -52,17 +29,25 @@ class CustomCollector(object):
     def collect(self):
         if self._jetson.ok():
             # Board info
-            i = InfoMetricFamily('jetson_info_board', 'Board sys info', labels=['board_info'])
-            i.add_metric(['info'], {
-                'machine': self._jetson.board['platform']['Machine'], 
-                'jetpack': self._jetson.board['hardware']['Jetpack'], 
+            board_info = InfoMetricFamily(
+                'jetson_info_board',
+                'Board sys info',
+                labels=['board_info']
+            )
+            board_info.add_metric(['info'], {
+                'machine': self._jetson.board['platform']['Machine'],
+                'jetpack': self._jetson.board['hardware']['Jetpack'],
                 'l4t': self._jetson.board['hardware']['L4T']
             })
-            yield i
+            yield board_info
 
             # Board hardware info
-            i = InfoMetricFamily('jetson_info_hardware', 'Board hardware info', labels=['board_hw'])
-            i.add_metric(['hardware'], {
+            hardware_info = InfoMetricFamily(
+                'jetson_info_hardware',
+                'Board hardware info',
+                labels=['board_hw']
+            )
+            hardware_info.add_metric(['hardware'], {
                 'model': self._jetson.board['hardware'].get('Model', 'unknown'),
                 'part_number': self._jetson.board['hardware'].get('699-level Part Number', 'unknown'),
                 'p_number': self._jetson.board['hardware'].get('P-Number', 'unknown'),
@@ -73,64 +58,92 @@ class CustomCollector(object):
                 'codename': self._jetson.board['hardware'].get('Codename', 'unknown'),
                 'serial_number': self._jetson.board['hardware'].get('Serial Number', 'unknown')
             })
-            yield i
+            yield hardware_info
 
-            # NV power mode
-            i = InfoMetricFamily('jetson_nvpmode', 'NV power mode', labels=['nvpmode'])
-            i.add_metric(['mode'], {'mode': self._jetson.nvpmodel.name})
-            yield i
+            # CPU Usage
+            cpu_gauge = GaugeMetricFamily(
+                name="cpu_usage",
+                documentation="CPU Usage from Jetson Stats",
+                labels=["core", "statistic"],
+                unit="%"
+            )
+            for core_number, core_data in enumerate(self._jetson.cpu['cpu']):
+                cpu_gauge.add_metric([str(core_number), "freq"], value=core_data["freq"]["cur"])
+                cpu_gauge.add_metric([str(core_number), "min_freq"], value=core_data["freq"]["min"])
+                cpu_gauge.add_metric([str(core_number), "max_freq"], value=core_data["freq"]["max"])
+                cpu_gauge.add_metric([str(core_number), "val"], value=core_data["idle"])
+            yield cpu_gauge
 
-            # System uptime
-            g = GaugeMetricFamily('jetson_uptime', 'System uptime', labels=['uptime'])
-            days = self._jetson.uptime.days
-            seconds = self._jetson.uptime.seconds
-            hours = seconds // 3600
-            minutes = (seconds // 60) % 60
-            g.add_metric(['days'], days)
-            g.add_metric(['hours'], hours)
-            g.add_metric(['minutes'], minutes)
-            yield g
+            # GPU Usage
+            gpu_gauge = GaugeMetricFamily(
+                name="gpu_utilization_percentage",
+                documentation="GPU Usage from Jetson Stats",
+                labels=["statistic", "nvidia_gpu"],
+                unit="%"
+            )
+            for gpu_name in self._jetson.gpu.keys():
+                gpu_gauge.add_metric([gpu_name, "freq"], value=self._jetson.gpu[gpu_name]["freq"]["cur"])
+                gpu_gauge.add_metric([gpu_name, "min_freq"], value=self._jetson.gpu[gpu_name]["freq"]["min"])
+                gpu_gauge.add_metric([gpu_name, "max_freq"], value=self._jetson.gpu[gpu_name]["freq"]["max"])
+            yield gpu_gauge
 
-            # CPU usage
-            g = GaugeMetricFamily('jetson_usage_cpu', 'CPU % schedutil', labels=['cpu'])
-            for cpu in self._jetson.cpu:
-                g.add_metric([cpu], self._jetson.cpu[cpu]['val'])
-            yield g
+            # RAM Usage
+            ram_gauge = GaugeMetricFamily(
+                name="ram_usage",
+                documentation="RAM Usage from Jetson Stats",
+                labels=["statistic"],
+                unit="kB"
+            )
+            ram_gauge.add_metric(["total"], value=self._jetson.ram['tot'])
+            ram_gauge.add_metric(["used"], value=self._jetson.ram['used'])
+            ram_gauge.add_metric(["free"], value=self._jetson.ram['free'])
+            yield ram_gauge
 
-            # GPU usage
-            g = GaugeMetricFamily('jetson_usage_gpu', 'GPU % schedutil', labels=['gpu'])
-            g.add_metric(['val'], self._jetson.gpu['val'])
-            yield g
+            # Swap Usage
+            swap_gauge = GaugeMetricFamily(
+                name="swap_usage",
+                documentation="Swap Usage from Jetson Stats",
+                labels=["statistic"],
+                unit="kB"
+            )
+            swap_gauge.add_metric(["total"], value=self._jetson.swap['tot'])
+            swap_gauge.add_metric(["used"], value=self._jetson.swap['used'])
+            yield swap_gauge
 
-            # RAM usage
-            g = GaugeMetricFamily('jetson_usage_ram', 'Memory usage', labels=['ram'])
-            g.add_metric(['shared'], self._jetson.ram['shared'])
-            yield g
+            # Disk Usage
+            disk_gauge = GaugeMetricFamily(
+                name="disk_usage",
+                documentation="Disk Usage from Jetson Stats",
+                labels=["mountpoint", "statistic"],
+                unit="GB"
+            )
+            for mountpoint, disk_info in self._jetson.disk.items():
+                if mountpoint == "/":
+                    disk_gauge.add_metric(["total"], value=disk_info["total"])
+                    disk_gauge.add_metric(["used"], value=disk_info["used"])
+                    disk_gauge.add_metric(["free"], value=disk_info["free"])
+            yield disk_gauge
 
-            # Disk usage
-            g = GaugeMetricFamily('jetson_usage_disk', 'Disk space usage', labels=['disk'])
-            g.add_metric(['used'], self._jetson.disk['used'])
-            g.add_metric(['total'], self._jetson.disk['total'])
-            g.add_metric(['available'], self._jetson.disk['available'])
-            g.add_metric(['available_no_root'], self._jetson.disk['available_no_root'])
-            yield g
+            # Uptime
+            uptime_gauge = GaugeMetricFamily(
+                name="uptime",
+                documentation="Uptime from Jetson Stats",
+                labels=["statistic"],
+                unit="s"
+            )
+            uptime_gauge.add_metric(["alive"], value=self._jetson.uptime.total_seconds())
+            yield uptime_gauge
 
-            # Fan usage
-            g = GaugeMetricFamily('jetson_usage_fan', 'Fan usage', labels=['fan'])
-            g.add_metric(['speed'], self._jetson.fan['speed'])
-            yield g
-
-            # Power usage
-            g = GaugeMetricFamily('jetson_usage_power', 'Power usage', labels=['power'])
-            for component in self._jetson.power[1]:
-                g.add_metric([component], self._jetson.power[1][component]['cur'])
-            yield g
-
-            # Sensor temperatures
-            g = GaugeMetricFamily('jetson_temperatures', 'Sensor temperatures', labels=['temperature'])
-            for temp_sensor in self._jetson.temperature:
-                g.add_metric([temp_sensor], self._jetson.temperature[temp_sensor])
-            yield g
+            # Temperature
+            temperature_gauge = GaugeMetricFamily(
+                name="temperature",
+                documentation="Temperature from Jetson Stats",
+                labels=["statistic", "machine_part"],
+                unit="C"
+            )
+            for part, temp in self._jetson.temperature.items():
+                temperature_gauge.add_metric([part], value=temp)
+            yield temperature_gauge
 
 # Funzione principale
 if __name__ == '__main__':
@@ -141,7 +154,7 @@ if __name__ == '__main__':
 
     # Avvia il server HTTP per esporre le metriche
     start_http_server(args.port)
-    REGISTRY.register(CustomCollector())
+    REGISTRY.register(CustomCollector(update_period=1))
     
     # Mantieni in esecuzione il programma per raccogliere metriche
     while True:
